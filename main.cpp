064a838a86be5052070d7fcf67a506b08af0d62a
Topias Liljegren <topias.liljegren@gmail.com>
2023-01-14 16:22:35 +0200

    cin, cout int ja muuta turhaa lisätty

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hei maailma" << std::endl;
+    std::string nimi;
+    std::cout << "Anna nimesi" << std::endl;
+    // std::cin >> nimi; // luetaan teksti ilman väliä
+    std::getline(std::cin,nimi); // luetaan teksti välien kanssa
+    std::cout << "Hei " << nimi <<std::endl;
+    
+    int age{23}; // declare and initialize
+    std::cout << age << std::endl;
+    age = 23; // assign
+
+    std::cerr << "Error" << std::endl;
+    std::clog << "Log" << std::endl;
+
     return 0;
 }
